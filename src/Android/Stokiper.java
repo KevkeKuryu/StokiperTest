5d3d9066a591ccb80aec65657488cfc4a6fd38a1
KevkeKuryu <130420929+KevkeKuryu@users.noreply.github.com>
2023-06-05 15:59:54 +0700

    Update Stokiper.java

--- src/Android/Stokiper.java
+++ src/Android/Stokiper.java
@@ -1,74 +1,36 @@
 package com.example.Stokiper;
 
 
-
-
 import org.apache.cordova.CallbackContext;
-
 import org.apache.cordova.CordovaPlugin;
-
 import org.apache.cordova.PluginResult;
-
 import org.json.JSONArray;
-
 import org.json.JSONException;
-
 import org.json.JSONObject;
 
 
-
-
 import com.rscja.barcode.BarcodeDecoder;
-
 import com.rscja.barcode.BarcodeFactory;
-
 import com.rscja.barcode.BarcodeUtility;
-
 import com.rscja.deviceapi.entity.BarcodeEntity;
 
-
-
-
 public class Stockiper extends CordovaPlugin {
-
     BarcodeDecoder barcode = BarcodeFactory.getInstance().getBarcodeDecoder();
 
-
-
-
     @Override
-
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
         if(action.equals("read"))
-
         {
-
             barcode.open();
-
             barcode.startScan();
-
            
-
             String hasil = "Scan berhasil";
-
             callbackContext.success(hasil);
-
             return true;
-
         }
-
-
-
-
         else
-
         {
-
             return false;
-
         }
-
     }
-
 }
